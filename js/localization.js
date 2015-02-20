dc82c159c00021ca9fd4f11a3ec0742c981993be
mpitman <mark.pitman@gettyimages.com>
2015-02-20 12:30:20 -0800

    auto-redirect for supported languages layout updates animated down arrow

--- js/localization.js
+++ js/localization.js
@@ -1,7 +1,33 @@
-var lang = navigator.languages ? navigator.languages[0].substring(0, 2) : 'en';
-if (lang !== = 'en') {
+(function() {
+  var supportedLanguages = ['de', 'es', 'fr', 'it', 'ja', 'nl', 'pt', 'sv', 'en', 'pseudo'];
+  var lang = 'en';
+  var preferredLang = navigator.languages.filter(function(l) {
+    return supportedLanguages.indexOf(l.substring(0, 2)) != -1;
+  })[0];
+
+  if (preferredLang) {
+    lang = preferredLang.substring(0, 2);
+  }
+
+  var needToRedirect = true;
   var langloc = '/' + lang;
-  if (window.location.toString().indexOf(langloc) < 0) {
+  var pathname = window.location.pathname;
+
+
+  for (i = 0; i < supportedLanguages.length; i++) {
+    if (pathname.indexOf(supportedLanguages[i]) > -1) {
+      needToRedirect = false;
+      break;
+    }
+  }
+
+  if (needToRedirect && supportedLanguages.indexOf(lang) > -1) {
+    needToRedirect = pathname.indexOf(langloc) < 0;
+  } else {
+    needToRedirect = false
+  }
+
+  if (needToRedirect) {
     window.location = langloc + window.location.pathname;
   }
-}
+})();
